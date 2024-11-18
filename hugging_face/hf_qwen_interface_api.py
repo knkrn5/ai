46b01a509ebb69ed64831705df9fc81d9bd7d9c7
Karan <xkaranx5@gmail.com>
2024-11-18 14:27:37 +0530

    loop added in qwen model

--- hugging_face/hf_qwen_interface_api.py
+++ hugging_face/hf_qwen_interface_api.py
@@ -6,21 +6,27 @@ load_dotenv()
 
 client = InferenceClient(api_key=os.getenv("HUGGING_FACE_API"))
 
-user_input = input("Ask the question or type 'exit' or 'q' to quit: ")
 
-messages = [
+while True:
+	user_input = input("\nAsk the question or type 'exit' or 'q' to quit: ")
+
+	if user_input.lower() in ["exit", 'q']:
+		print("Exiting...")
+		break
+
+	messages = [
 	{
 		"role": "user",
 		"content": user_input
 	}
-]
+	]
 
-stream = client.chat.completions.create(
-    model="Qwen/Qwen2.5-1.5B-Instruct", 
-	messages=messages, 
-	max_tokens=500,
-	stream=True
-)
+	stream = client.chat.completions.create(
+		model="Qwen/Qwen2.5-1.5B-Instruct", 
+		messages=messages, 
+		max_tokens=200,
+		stream=True
+	)
 
-for chunk in stream:
-    print(chunk.choices[0].delta.content, end="")
+	for chunk in stream:
+		print(chunk.choices[0].delta.content, end="")
