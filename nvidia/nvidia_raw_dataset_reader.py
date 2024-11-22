ec0799abb7258278805791db6b6f19c7b7f720b9
Karan <xkaranx5@gmail.com>
2024-11-22 13:16:15 +0530

    nvidia dataset reader added

---
+++ nvidia/nvidia_raw_dataset_reader.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from datasets import load_dataset
+
+load_dotenv()
+
+# Load the dataset
+dataset = load_dataset("knkrn5/wealthpsychology-raw-data")
+
+# Checking if the data has the tokenized format
+print(dataset)
+
+all_data = []
+
+# Print details of each split in the DatasetDict
+for split_name, split_data in dataset.items():
+    split_df = split_data.to_pandas()
+    all_data.append(split_df)
+    print(split_df.head())
+    print("\n" + "="*50 + "\n")
+    
+
+# Initialize the OpenAI client with NVIDIA's base URL and API key
+client = OpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=os.getenv("NVIDIA_API")
+)
+
+while True:
+    # Take user input for the question
+    user_question = input("\nPlease enter your question (or type 'exit' or 'q' to quit): ")
+
+    # Exit the loop if the user types 'exit'
+    if user_question.lower() in ['exit', 'q']:
+        print("Exiting the program.")
+        break
+
+    # Create a completion request with the user question
+    completion = client.chat.completions.create(
+        model="nvidia/llama-3.1-nemotron-70b-instruct",
+        messages=[
+            {"role": "system", "content": " your are an AI assistant."},
+            {"role": "system", "content": f"dataset: {all_data}."},
+            {"role": "user", "content": user_question}
+        ],
+        temperature=0.5,
+        # top_k = 50,
+        top_p=0.7,
+        max_tokens=1024,
+        # repetition_penalty=1.2,
+        stream=True
+    )
+
+    # Stream the response chunks and print them
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
--- tokenization/wp_csv_data_tokenization.py
+++ tokenization/wp_csv_data_tokenization.py
@@ -100,7 +100,7 @@ DESCRIPTION = "This dataset contains tokenized data for Wealth Psychology websit
 # Push the dataset to the Hugging Face Hub
 tokenized_data.push_to_hub(
     repo_id=DATASET_NAME, 
-    # repo_id="your_huggingface_username/your_dataset_name", 
+    # repo_id="knkrn5/your_dataset_name", 
     private=False,         
     token=os.getenv("HUGGING_FACE_WRITE_API")  
 ) """
